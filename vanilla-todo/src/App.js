b359a437628beff36f913fbbe54ca01c4e280a99
chchaeun <kce3615@naver.com>
2023-02-11 16:25:19 +0900

    feat: children 멤버변수 제거

--- vanilla-todo/src/App.js
+++ vanilla-todo/src/App.js
@@ -4,9 +4,17 @@ import Title from "./components/Title.js";
 import AddForm from "./components/AddForm.js";
 import TodoList from "./components/TodoList.js";
 
-class App extends Component {}
+class App extends Component {
+  html() {
+    return `
+            <header id="Title"></header>
+            <div id="AddForm"></div>
+            <ul id="TodoList"></ul>
+        `;
+  }
+}
 
-new App(document.querySelector("#App"), ["Title", "AddForm", "TodoList"]);
+new App(document.querySelector("#App"));
 
 export const title = new Title(document.querySelector("#Title"));
 export const addForm = new AddForm(document.querySelector("#AddForm"));
--- vanilla-todo/src/Component.js
+++ vanilla-todo/src/Component.js
@@ -1,10 +1,8 @@
 export default class Component {
   $root;
-  #children;
   #state;
-  constructor($root, children) {
+  constructor($root) {
     this.$root = $root;
-    this.#children = children;
     this.setState(this.initState());
     this.render();
     this.setEvent();
@@ -21,18 +19,8 @@ export default class Component {
   html() {
     return `<div></div>`;
   }
-  template() {
-    if (this.#children) {
-      const childNodes = this.#children
-        .map((child) => `<div id="${child}"></div>`)
-        .join("");
-
-      return this.html() + childNodes;
-    }
-    return this.html();
-  }
   render() {
-    this.$root.innerHTML = this.template();
+    this.$root.innerHTML = this.html();
   }
   setEvent() {}
   addEvent(eventType, targetSelector, callback) {
