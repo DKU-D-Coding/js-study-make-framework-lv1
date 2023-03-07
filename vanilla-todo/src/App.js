fd7ea326ce56d3dbd080c4c1a8d7167ff951d169
Kim Chae Eun <85024598+chchaeun@users.noreply.github.com>
2023-03-07 13:44:12 +0900

    fix: console.log 제거

--- vanilla-todo/src/App.js
+++ vanilla-todo/src/App.js
@@ -3,10 +3,8 @@ import Title from "./components/Title.js";
 import AddForm from "./components/AddForm.js";
 import TodoList from "./components/TodoList.js";
 import { CONTAINER } from "./constants/_index.js";
-import { store } from "./store.js";
 class App extends Component {
   html() {
-    console.log(store.getState().todos, store.getState().editing);
     return `
       <header id="${CONTAINER.TITLE}">${Title()}</header>
       <div id="${CONTAINER.ADD_FORM}"></div>
